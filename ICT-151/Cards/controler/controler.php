4a444fb3da96f3d5f0ee05df60fb79e86e0a585f
Pedro Pinheiro <pedroyverdon@gmail.com>
2022-02-16 11:36:47 +0100

    +Login/Logout session and snows display mode

--- ICT-151/Cards/controler/controler.php
+++ ICT-151/Cards/controler/controler.php
@@ -13,7 +13,7 @@
  */
 
 function home(){
-
+    $_GET['action'] = "home";
     require "view/home.php";
 }
 
@@ -25,28 +25,65 @@ function home(){
  * One for the seller, an other one for the customer.
  */
 function displaySnows(){
-
-    require_once "model/model.php";
+    require_once "model/snowsManager.php";
     $snowsResults = getSnows();
-    require "view/snows.php";
-
+    $usertype=$_SESSION['userType'];
+    if($usertype==1){
+        require "view/snows_admin.php";
+    }
+    else{
+        require "view/snows.php";
+    }
 }
 
-function getASnow($snow_code){
 
-    $strgSeparator = '\'';
 
-    $snowQuery = 'SELECT code, brand, model, snowLength, dailyPrice, qtyAvailable, description, photo FROM snows WHERE code='.$strgSeparator.$snow_code.$strgSeparator.'AND active=1';
-
-    $snowResults = executeQuerySelect($snowQuery);
-
-    return $snowsResults;
-
+/**
+ * This function is designed to get only one snow results (for aSnow view)
+ * @param none
+ */
+function displayASnow($snow_code){
+    require_once "../model/snowsManager.php";
+    $snowsResults= getASnow($snow_code);
+    require "../view/snow_detail.php";
 }
 
-function displayASnow(){
-    require_once "model/model.php";
-    $snowsResults = getASnow();
-    require "view/snows_detail.php";
+function login($loginRequest){
+    //if a login request was submitted
+    if (isset($loginRequest['inputUserEmailAddress']) &&
+        isset($loginRequest['inputUserPsw'])) {
+        //extract login parameters
+        $userEmailAddress = $loginRequest['inputUserEmailAddress'];
+        $userPsw = $loginRequest['inputUserPsw'];
+        //try to check if user/psw are matching with the database
+        require_once "model/usersManager.php";
+        if (isLoginCorrect($userEmailAddress, $userPsw)) {
+            createSession($userEmailAddress);
+            $_GET['loginError'] = false;
+            $_GET['action'] = "home";
+            require "view/home.php";
+        } else { //if the user/psw does not match, login form appears again
+            $_GET['loginError'] = true;
+            $_GET['action'] = "login";
+            require "view/login.php";
+        }
+    }else{ //the user hasn’t yet filled the form
+        $_GET['action'] = "login";
+        require "view/login.php";
+    }
 }
-
+function logout(){
+    session_start();
+    session_destroy();
+    header("Location:index.php");
+}
+/**
+ * This function is designed to create a new user session
+ * @param $userEmailAddress : user unique id
+ */
+function createSession($userEmailAddress){
+    $_SESSION['userEmailAddress'] = $userEmailAddress;
+    //set user type in Session
+    $userType = getUserType($userEmailAddress);
+    $_SESSION['userType'] = $userType;
+}
