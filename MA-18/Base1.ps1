dcdb2be76b558bec4202efc753ff610d18caa882
Tugakit <tuga.kit@gmail.com>
2022-03-01 15:35:26 +0100

    Last exercices made
    Folder/Files manipulation from powershell script + Advanced way to ask folder destination on Base3.ps1

---
+++ MA-18/Base1.ps1
@@ -0,0 +1,18 @@
+﻿cd C:\
+
+# Si le dossier ESSAIS est encore présent, dans le cas ou il en a pas de dossier l'erreur est ignorée
+Remove-Item \ESSAIS\ -Recurse -ErrorAction Ignore
+
+New-Item \ESSAIS -ItemType "directory"
+New-Item \ESSAIS\A -ItemType "directory"
+New-Item \ESSAIS\B -ItemType "directory"
+New-Item \ESSAIS\B\B1 -ItemType "directory"
+New-Item \ESSAIS\B\B2 -ItemType "directory"
+New-Item \ESSAIS\B\B2A -ItemType "directory"
+New-Item \ESSAIS\B\B2B -ItemType "directory"
+New-Item \ESSAIS\B\B2C -ItemType "directory"
+
+# Ajout d'un fichier texte dans le dossier racine ESSAIS
+New-Item \ESSAIS\ESSAI_1.txt
+Copy-Item \ESSAIS\ESSAI_1.txt \ESSAIS\COPIE_ESSAI.txt
+Copy-Item \ESSAIS\COPIE_ESSAI.txt \ESSAIS\TEST_1.TXT
---
+++ MA-18/Base3.ps1
@@ -0,0 +1,33 @@
+﻿Function Get-Folder($initialDirectory = "")
+{
+    [System.Reflection.Assembly]::LoadWithPartialName("System.windows.forms") | Out-Null
+
+    $foldername = New-Object System.Windows.Forms.FolderBrowserDialog #Création de la fenetre de selection du dossier de destination
+    $foldername.Description = "Select a folder"
+    $foldername.rootfolder = "MyComputer"
+    $foldername.SelectedPath = $initialDirectory
+
+    if ($foldername.ShowDialog() -eq "OK") {
+        $folder += $foldername.SelectedPath
+    }
+    return $folder
+}
+
+$a = Get-Folder
+cd $a
+
+Remove-Item ESSAIS\ -Recurse -ErrorAction Ignore
+
+New-Item ESSAIS -ItemType "directory"
+New-Item ESSAIS\A -ItemType "directory"
+New-Item ESSAIS\B -ItemType "directory"
+New-Item ESSAIS\B\B1 -ItemType "directory"
+New-Item ESSAIS\B\B2 -ItemType "directory"
+New-Item ESSAIS\B\B2A -ItemType "directory"
+New-Item ESSAIS\B\B2B -ItemType "directory"
+New-Item ESSAIS\B\B2C -ItemType "directory"
+
+# Ajout d'un fichier texte dans le dossier racine ESSAIS
+New-Item ESSAIS\ESSAI_1.txt
+Copy-Item ESSAIS\ESSAI_1.txt ESSAIS\COPIE_ESSAI.txt
+Copy-Item ESSAIS\COPIE_ESSAI.txt ESSAIS\TEST_1.TXT
