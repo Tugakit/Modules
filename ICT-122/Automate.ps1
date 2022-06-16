ed05467bf31c6e8acb0489adad663028bc29354b
Tugakit <tuga.kit@gmail.com>
2022-06-16 12:08:38 +0200

    New docs

---
+++ ICT-122/Automate.ps1
@@ -0,0 +1,28 @@
+﻿#Variables globales
+$Money = 0
+$Price = 0
+$Position = 0
+
+function IsNum($Args) {
+
+   $res = $Args -match "^[-+]?([0-9]*\.?[0-9]*)$"
+        
+   return $res
+}
+
+function AddMoney($Args){
+   if (isNum){
+    $Money+=$Arg[0]
+    return $Money
+   }
+} 
+
+function Select(){
+param(
+[Parameter()] [Float]$Select
+)
+$Position = $Select
+return $Select
+}
+$Position = $Select
+Write-Host " Hello $Position"
